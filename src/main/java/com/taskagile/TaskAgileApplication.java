d330610cc3641f1549587113d7e46c985bf4cef4
davidpaulkim <davidpaulkim@gmail.com>
2020-08-14 13:13:11 +0900

    추가시도 코멘트 추가

--- src/main/java/com/taskagile/TaskAgileApplication.java
+++ src/main/java/com/taskagile/TaskAgileApplication.java
@@ -3,7 +3,7 @@ package com.taskagile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-
+// OUATH 추가 시도
 @ServletComponentScan(
   basePackages = {"com.taskagile.infrastructure.file.local"}
 )
